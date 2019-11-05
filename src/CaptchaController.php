15d3444cffbdc0842058c38eb81ae1705c28607b
shirne <shirne@126.com>
2019-11-05 15:49:39 +0800

    init

---
+++ src/CaptchaController.php
@@ -0,0 +1,25 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2015 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: yunwuxin <448901948@qq.com>
+// +----------------------------------------------------------------------
+
+namespace think\captcha;
+
+use think\facade\Config;
+
+class CaptchaController
+{
+    public function index($id = "")
+    {
+        $config = (array) Config::pull('captcha');
+        $mode = isset($config['mode'])?$config['mode']:'';
+        $captcha = new Captcha($config, $mode=='cache'? think\facade\Cache::instance():null );
+        return $captcha->entry($id);
+    }
+}
---
+++ src/helper.php
@@ -0,0 +1,61 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2015 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: yunwuxin <448901948@qq.com>
+// +----------------------------------------------------------------------
+
+Route::get('captcha/[:id]', "\\think\\captcha\\CaptchaController@index");
+
+Validate::extend('captcha', function ($value, $id = '') {
+    return captcha_check($value, $id);
+});
+
+Validate::setTypeMsg('captcha', ':attribute错误!');
+
+/**
+ * @param string $id
+ * @param array  $config
+ * @return \think\Response
+ */
+function captcha($id = '', $config = [], $store = null)
+{
+    $captcha = new \think\captcha\Captcha($config, $store);
+    return $captcha->entry($id);
+}
+
+/**
+ * @param $id
+ * @return string
+ */
+function captcha_src($id = '')
+{
+    return Url::build('/captcha' . ($id ? "/{$id}" : ''));
+}
+
+/**
+ * @param $id
+ * @return mixed
+ */
+function captcha_img($id = '')
+{
+    return '<img src="' . captcha_src($id) . '" alt="captcha" />';
+}
+
+/**
+ * @param        $value
+ * @param string $id
+ * @param array  $config
+ * @return bool
+ */
+function captcha_check($value, $id = '', $store=null)
+{
+    $config = (array) Config::pull('captcha');
+    $mode = isset($config['mode'])?$config['mode']:'';
+    $captcha = new \think\captcha\Captcha($config, $mode=='cache'? think\facade\Cache::instance():null);
+    return $captcha->check($value, $id);
+}
